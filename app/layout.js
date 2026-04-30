d1f7887105b20827bbdf629738f7b43888cf526d
IrisVentos <irisv8949@gmail.com>
2026-04-30 16:07:06 +0200

    change icon of website

--- app/layout.js
+++ app/layout.js
@@ -24,6 +24,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr" className={`${outfit.variable} ${spaceGrotesk.variable} h-full antialiased`}>
+      <head>
+        <link rel='icon' type='image/x-icon' href='logo_alma.png' ></link>
+      </head>
       <body className="min-h-full flex flex-col bg-[#0a0f1e] text-white font-outfit">
           <div className="relative z-10 flex flex-col flex-1" />
           <Navbar />
